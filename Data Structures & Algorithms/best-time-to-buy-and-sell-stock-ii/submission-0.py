82f737ebb3273cef88633bc71f88225de9898297
Harshit <160395320+Harshit2546@users.noreply.github.com>
2026-07-06 12:05:02 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        
+        # Start from the second day
+        for i in range(1, len(prices)):
+            # If the price increased compared to yesterday, take the profit
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+                
+        return max_profit
